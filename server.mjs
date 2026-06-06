ef389c1cdc4b59c84082255da1f57876ffb3d16d
cortez-devs <cortezcr@oregonstate.edu>
2026-06-06 16:00:14 -0500

    Add CORS for frontend API coms

--- server.mjs
+++ server.mjs
@@ -3,11 +3,13 @@ import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const imagesDir = path.join(__dirname, 'images');
